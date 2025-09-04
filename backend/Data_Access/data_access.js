0da392cf30d53baaa38d37015df5529a53cbb683
ABF00711 <lwei99597@gmail.com>
2025-09-05 05:27:39 +1000

    feat(data_access): add Data_Access/data_access.js

---
+++ backend/Data_Access/data_access.js
@@ -0,0 +1,47 @@
+class DataAccess{
+    constructor(dbModel){
+        this.dbModel = dbModel;
+    }
+
+    async create (data) {
+        try {
+            await this.dbModel.create(data);
+        } catch (error) {
+            console.log("DA_createError: ", error);
+        }
+    }
+
+    async getAllData () {
+        try {
+            return await this.dbModel.find();
+        } catch (error) {
+            console.log("DA_getAllDataError: ", error)
+        }
+    }
+
+    async searchUsers(query) {
+        return await this.dbModel.find({
+            $or: [
+            { name: { $regex: query, $options: "i" } },     // case-insensitive
+            { display_name: { $regex: query, $options: "i" } },
+            { job: { $regex: query, $options: "i" } }
+            ]
+        });
+    }
+
+    async update (id, newData){
+        try {
+            return await this.dbModel.findByIdAndUpdate(id, newData, {new:true});
+        } catch (error) {
+            console.log("DA_updateErro: ", error);
+        }
+    }
+
+    async delete (id){
+        try {
+            await this.dbModel.findByIdAndDelete(id);
+        } catch (error) {
+            console.log()
+        }
+    }
+}
