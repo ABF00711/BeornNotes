e957ad431e9c3858d904fead009062e86059372e
ABF00711 <lwei99597@gmail.com>
2025-09-06 22:22:15 +1000

    refactor(register):  complete toast effect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Register from './Pages/Register';
 import Login from './Pages/Login';
 import ContextProvider from './Context';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
             <Route path="/register" element={<Register />} />
             {/* <Route path="/dashboard" element={<Dashboard />} /> */}
           </Routes>
+          <ToastContainer />
         </ContextProvider>
       </BrowserRouter>
     </div>
--- frontend/src/Hooks/useAuth.js
+++ frontend/src/Hooks/useAuth.js
@@ -2,7 +2,6 @@ import React, { useContext } from "react";
 import { MyContext } from "../Context";
 import services from "../Services";
 import {toast} from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
 
 function useAuth(){
     const {userData, setUserData, token, setToken} = useContext(MyContext);
@@ -13,10 +12,10 @@ function useAuth(){
             if (res.message == "register success"){
                 setUserData(res.user);
                 setToken(res.token);
-                toast.success(res.message);
+                toast.success(res.message, {position: "top-right"});
                 return true;
             }
-            toast.error(res.message);
+            toast.error(res.message, {position: "top-right"});
             return false;
         } catch (error) {
             console.log("registerError: ", error);
