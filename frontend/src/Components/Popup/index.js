95c898df32436367f6b9cc1d4b0a848acad111df
ABF00711 <lwei99597@gmail.com>
2025-10-08 06:46:21 +1100

    refactor(InputerGrou, Popup): modify to move mandatory to the left of fields

--- frontend/src/Components/Popup/index.js
+++ frontend/src/Components/Popup/index.js
@@ -63,7 +63,7 @@ function Popup({ fieldFormat, value, handleChange, options = [], submitted = fal
     return (
         <div className="field row" ref={popupRef}>
             <label htmlFor={fieldFormat.field_name} className="field-label">
-                {fieldFormat.field_label}
+                {fieldFormat.field_label}{fieldFormat.mandatory ? " *" : ""}
             </label>
             <div className="popup-container">
                 <div 
@@ -115,7 +115,6 @@ function Popup({ fieldFormat, value, handleChange, options = [], submitted = fal
                 </div>
             )}
             </div>
-            {fieldFormat.mandatory ? " *" : ""}
         </div>
     );
 }
