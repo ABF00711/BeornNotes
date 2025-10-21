9b12ea2ac1ed13c1d3a775287b08b2b4382333d2
ABF00711 <lwei99597@gmail.com>
2025-10-22 00:17:50 +1100

    refactor(App.js): remove AG grid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,6 @@ import ContextProvider from './Context';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Dashboard from './Pages/Dashboard';
-import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
-import { AllEnterpriseModule, LicenseManager } from "ag-grid-enterprise";
 import Orders from './Pages/Orders';
 import InventoryReport from './Pages/Reports/InventoryReport';
 import SummaryReport from './Pages/Reports/SummaryReport';
@@ -18,9 +16,6 @@ import Customers from './Pages/Customers';
 import Layout from './Pages/Layout';
 
 // Register all Community features
-ModuleRegistry.registerModules([AllCommunityModule, AllEnterpriseModule]);
-LicenseManager.setLicenseKey("Using_this_{AG_Charts_and_AG_Grid}_Enterprise_key_{AG-103811}_in_excess_of_the_licence_granted_is_not_permitted___Please_report_misuse_to_legal@ag-grid.com___For_help_with_changing_this_key_please_contact_info@ag-grid.com___{Beornsoft,_LLC}_is_granted_a_{Multiple_Applications}_Developer_License_for_{1}_Front-End_JavaScript_developer___All_Front-End_JavaScript_developers_need_to_be_licensed_in_addition_to_the_ones_working_with_{AG_Charts_and_AG_Grid}_Enterprise___This_key_has_been_granted_a_Deployment_License_Add-on_for_{1}_Production_Environment___This_key_works_with_{AG_Charts_and_AG_Grid}_Enterprise_versions_released_before_{20_October_2026}____[v3]_[0102]_MTc5MjQ1MDgwMDAwMA==4696f1d596c6d71813a6b8c15f1940e5");
-
 if (window.Smart) {
   window.Smart.License = "0A2C72B9-D78F-5E17-8D07-0CBC0E1EDC29";
 } else {
