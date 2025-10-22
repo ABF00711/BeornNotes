0741a49af95a0edc6c16b61604cf3320a7e9b96e
ABF00711 <lwei99597@gmail.com>
2025-10-23 10:58:50 +1100

    feat(Documents): add Document component

---
+++ frontend/src/Components/Documents/index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+
+function Documents (){
+
+    return (
+        <div className="documents">
+            <div className="documents-main">
+
+            </div>
+        </div>
+    );
+}
+
+export default Documents;
