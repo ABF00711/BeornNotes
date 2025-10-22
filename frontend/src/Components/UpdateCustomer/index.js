2b9165ebc90ddb02bbddd72458182ade27bb3944
ABF00711 <lwei99597@gmail.com>
2025-10-23 10:56:57 +1100

    feat(UpdateCustomer): add UpdateCustomer component

---
+++ frontend/src/Components/UpdateCustomer/index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+
+function UpdateCustomer(){
+
+    return (
+        <div className="updateCustomer">
+            <div className="updateCustomer-main">
+
+            </div>
+        </div>
+    );
+}
+
+export default UpdateCustomer;
