55f759058274f3218bbd0e6043d907cd30d198eb
ABF00711 <lwei99597@gmail.com>
2025-10-24 16:02:58 +1100

    feat(documentController.js)

---
+++ backend/Controller/documentController.js
@@ -0,0 +1,53 @@
+const mysqlDA = require("../Data_Access");
+const documentService = require("../Service/documents");
+
+const documentController = {
+    getDocuments: async(req, res) => {
+        try {
+            const {customerId} = req.body;
+            const formData = await mysqlDA.getData("forms", {table_name: "documents"});
+            const sql = `${formData.Select} Where customer = ${customerId} Order by ${formData.OrderBy}`;
+            const documents = await mysqlDA.excuteSql(sql);
+            res.json({message: "getDocuments success", documents});
+        } catch (error) {
+            console.log("getDocumentsError: ", error);
+            res.json({message: "getDocuments failed"});
+        }
+    },
+
+    createDocument: async(req, res) => {
+        try {
+            const {document, documentInfo} = req.body;
+
+            const id = await mysqlDA.create("documents", documentInfo);
+
+            const documentUrl = await documentService.uploadDocument(document, id);
+            documentInfo.id = id;
+            documentInfo.documentUrl = documentUrl;
+
+            await mysqlDA.update("documents", documentInfo);
+
+            res.json({message: "createDocument success", document: documentInfo});
+        } catch (error) {
+            console.log("createDocumentError: ", error);
+        }  
+    },
+
+    updateDocument: async (req, res) => {
+        try {
+            
+        } catch (error) {
+            console.log("updateDocumentError: ", error);
+        }
+    },
+
+    deleteDocument: async (req, res) => {
+        try {
+            
+        } catch (error) {
+            console.log("deleteDocumentError: ", error);
+        }
+    },
+}
+
+module.exports = documentController;
