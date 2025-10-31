b6b78734131f207202c3281f4fa50c12bd09dea5
ABF00711 <lwei99597@gmail.com>
2025-10-31 21:47:22 +0900

    refactor(documentController): modify deleteDocuments to send json

--- backend/Controller/documentController.js
+++ backend/Controller/documentController.js
@@ -2,20 +2,20 @@ const mysqlDA = require("../Data_Access");
 const documentService = require("../Service/documents");
 
 const documentController = {
-    getDocuments: async(req, res) => {
+    getDocuments: async (req, res) => {
         try {
-            const {customerId} = req.body;
-            const formData = await mysqlDA.getOneData("forms", {TableView: "documents"})
+            const { customerId } = req.body;
+            const formData = await mysqlDA.getOneData("forms", { TableView: "documents" })
             const sql = `${formData.Select} Where customer = ${customerId} Order by ${formData.OrderBy}`;
             const documents = await mysqlDA.excuteSql(sql);
-            res.json({message: "getDocuments success", documents});
+            res.json({ message: "getDocuments success", documents });
         } catch (error) {
             console.log("getDocumentsError: ", error);
-            res.json({message: "getDocuments failed"});
+            res.json({ message: "getDocuments failed" });
         }
     },
 
-    createDocument: async(req, res) => {
+    createDocument: async (req, res) => {
         try {
             const document = req.file;
             const documentInfo = JSON.parse(req.body.fileInfo);
@@ -28,10 +28,10 @@ const documentController = {
 
             await mysqlDA.update("documents", documentInfo);
 
-            res.json({message: "createDocument success", document: documentInfo});
+            res.json({ message: "createDocument success", document: documentInfo });
         } catch (error) {
             console.log("createDocumentError: ", error);
-        }  
+        }
     },
 
     updateDocument: async (req, res) => {
@@ -39,16 +39,16 @@ const documentController = {
             const document = req.file;
             const documentInfo = JSON.parse(req.body.fileInfo);
             await mysqlDA.update("documents", documentInfo);
-            
-            if(!document){
-                res.json({message: "updateDocument success"});
+
+            if (!document) {
+                res.json({ message: "updateDocument success" });
                 return;
             }
             const key = documentInfo.documentUrl.split("com/")[1];
 
             await documentService.updateDocument(document, key);
 
-            res.json({message: "updateDocument success"});
+            res.json({ message: "updateDocument success" });
         } catch (error) {
             console.log("updateDocumentError: ", error);
         }
@@ -56,20 +56,16 @@ const documentController = {
 
     deleteDocuments: async (req, res) => {
         try {
-            const {selectedRows} = req.body;
+            const { selectedRows } = req.body;
 
             const ids = selectedRows.map((row) => row.id);
-            const keys = selectedRows.map((row) => {return {Key: row.documentUrl?.split("com/")[1]}});
+            const keys = selectedRows.map((row) => { return { Key: row.documentUrl?.split("com/")[1] } });
 
             const sql = `Delete From documents Where id In (${ids.join(", ")})`;
             await mysqlDA.excuteSql(sql);
-            
+
             const result = documentService.deleteDocuments(keys);
-            if(result){
-                res.json({ message: "deleteDocuments success" });
-            } else {
-                res.json({ message: "deleteDocuments failed" });
-            }
+            res.json({ message: "deleteDocuments success" });
         } catch (error) {
             console.log("deleteDocumentError: ", error);
         }
@@ -77,9 +73,9 @@ const documentController = {
 
     getUrl: async (req, res) => {
         try {
-            const {key} = req.body;
+            const { key } = req.body;
             const signedUrl = await documentService.getSignedUrl(key);
-            res.json({message: "getUrl success", signedUrl});
+            res.json({ message: "getUrl success", signedUrl });
         } catch (error) {
             console.log("getUrlError: ", error);
         }
