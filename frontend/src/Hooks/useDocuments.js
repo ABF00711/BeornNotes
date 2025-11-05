37d4e01c09ebd6f1db5b8b4dcc32efc745aa93c5
ABF00711 <lwei99597@gmail.com>
2025-11-06 05:03:29 +0900

    refactor(useDocuments): add openDocument functi8on

--- frontend/src/Hooks/useDocuments.js
+++ frontend/src/Hooks/useDocuments.js
@@ -4,13 +4,13 @@ import services from "../Services";
 const { useContext } = require("react");
 const { MyContext } = require("../Context");
 
-function useDocuments () {
-    const {documents, setDocuments} = useContext(MyContext);
+function useDocuments() {
+    const { documents, setDocuments } = useContext(MyContext);
 
     const getDocuments = async (customer) => {
         try {
             const res = await services.getDocuments(customer);
-            if(res.message == "getDocuments success"){
+            if (res.message == "getDocuments success") {
                 setDocuments(res.documents);
                 return;
             }
@@ -18,12 +18,12 @@ function useDocuments () {
             console.log("getDocumentsError: ", error);
         }
     }
-    
+
     const getOneDocument = async (documentInfo) => {
         try {
             const key = documentInfo.documentUrl.split("com/")[1];
             const res = await services.getOneDocument(key);
-            if(res.message == "getUrl success"){
+            if (res.message == "getUrl success") {
                 return res.signedUrl;
             }
         } catch (error) {
@@ -37,7 +37,7 @@ function useDocuments () {
             formData.append("file", document);
             formData.append("fileInfo", JSON.stringify(documentInfo));
             const res = await services.createDocument(formData);
-            if(res.message == "createDocument success"){
+            if (res.message == "createDocument success") {
                 setDocuments(documents => [...documents, res.document]);
                 toast(res.message);
                 return;
@@ -46,15 +46,15 @@ function useDocuments () {
             console.log("createDocumentError: ", error);
         }
     }
-    const updateDocument = async(documentFile, documentInfo) => {
+    const updateDocument = async (documentFile, documentInfo) => {
         try {
             const formData = new FormData();
             formData.append("file", documentFile);
             formData.append("fileInfo", JSON.stringify(documentInfo));
             const res = await services.updateDocument(formData);
-            if(res.message == "updateDocument success"){
-                setDocuments(documents.map(document => 
-                    document.id === documentInfo.id ? {...document, name: documentInfo.name, description: documentInfo.description} : document
+            if (res.message == "updateDocument success") {
+                setDocuments(documents.map(document =>
+                    document.id === documentInfo.id ? { ...document, name: documentInfo.name, description: documentInfo.description } : document
                 ));
                 toast.success("Document updated successfully!");
             }
@@ -65,7 +65,7 @@ function useDocuments () {
     const deleteDocuments = async (selectedRows) => {
         try {
             const res = await services.deleteDocuments(selectedRows);
-            if(res.message == "deleteDocuments success"){
+            if (res.message == "deleteDocuments success") {
                 let _documents = [...documents];
                 selectedRows.map((selectedRow) => {
                     _documents = _documents.filter((document) => document.id !== selectedRow.id);
@@ -78,13 +78,19 @@ function useDocuments () {
         }
     }
 
+    const openDocument = async (row) => {
+        const signedUrl = await getOneDocument(row);
+        window.open(signedUrl);
+    }
+
     return ({
         documents,
         getDocuments,
         createDocument,
         updateDocument,
         deleteDocuments,
-        getOneDocument
+        getOneDocument,
+        openDocument
     });
 }
 
