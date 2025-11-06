43d87fd01cd0905880e4b5f073672ebf03513a7c
ABF00711 <lwei99597@gmail.com>
2025-11-06 14:45:41 +0900

    refactor(useCustomTable): modify to call services

--- frontend/src/Hooks/useCustomTable.js
+++ frontend/src/Hooks/useCustomTable.js
@@ -1,31 +1,33 @@
+import services from "../Services";
+
 const useCustomTable = () => {
 
-    const saveGridState = async(state, formName) => {
-    try {
-      const res = await services.createGridState(state, formName);
-      if(res.message == "createGridState success"){
-        console.log(res.message);
-      }
-    } catch (error) {
-      console.log("saveGridState: ", error);
+    const saveGridState = async (state, formName) => {
+        try {
+            const res = await services.createGridState(state, formName);
+            if (res.message == "createGridState success") {
+                console.log(res.message);
+            }
+        } catch (error) {
+            console.log("saveGridState: ", error);
+        }
     }
-  }
 
-  const getGridState = async (formName) => {
-    try {
-      const res = await services.getGridState(formName);
-      if(res.message == "getGridState success"){
-        return res.state;
-      }
-      return "";
-    } catch (error) {
-      console.log("getGridState: ", error);
+    const getGridState = async (formName) => {
+        try {
+            const res = await services.getGridState(formName);
+            if (res.message == "getGridState success") {
+                return res.state;
+            }
+            return "";
+        } catch (error) {
+            console.log("getGridState: ", error);
+        }
     }
-  }
 
-  return ({
-    saveGridState, getGridState
-  })
+    return ({
+        saveGridState, getGridState
+    })
 
 }
 
