17a967e6688fcec5a49c2aa30b9ddd9a3d0777af
ABF00711 <lwei99597@gmail.com>
2025-11-06 09:37:00 +0900

    feat(useCutomTable): add useCustomTable hook

---
+++ frontend/src/Hooks/useCustomTable.js
@@ -0,0 +1,32 @@
+const useCustomTable = () => {
+
+    const saveGridState = async(state, formName) => {
+    try {
+      const res = await services.createGridState(state, formName);
+      if(res.message == "createGridState success"){
+        console.log(res.message);
+      }
+    } catch (error) {
+      console.log("saveGridState: ", error);
+    }
+  }
+
+  const getGridState = async (formName) => {
+    try {
+      const res = await services.getGridState(formName);
+      if(res.message == "getGridState success"){
+        return res.state;
+      }
+      return "";
+    } catch (error) {
+      console.log("getGridState: ", error);
+    }
+  }
+
+  return ({
+    saveGridState, getGridState
+  })
+
+}
+
+export default useCustomTable;
