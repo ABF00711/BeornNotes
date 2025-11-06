6f16d86028a5f2be13e6a5896530e586c22bd361
ABF00711 <lwei99597@gmail.com>
2025-11-07 05:38:43 +0900

    refactor(SmartGrid): modify areArrayEqual to fix undefined error

--- frontend/src/Components/SmartGrid/index.js
+++ frontend/src/Components/SmartGrid/index.js
@@ -96,10 +96,10 @@ function SmartGrid(props) {
 }
 
 function areArraysEqual(arr1, arr2) {
-    if (arr1.length !== arr2.length) {
+    if (arr1?.length !== arr2?.length) {
         return false;
     }
-    for (let i = 0; i < arr1.length; i++) {
+    for (let i = 0; i < arr1?.length; i++) {
         if (JSON.stringify(arr1[i]) !== JSON.stringify(arr2[i])) {
             return false;
         }
