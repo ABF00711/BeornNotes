31a75f956ed96e9a9c434e3c7f1b642f57f0568a
ABF00711 <lwei99597@gmail.com>
2025-11-08 03:08:14 +0900

    refactor(useSmartGrid): modify getSmartColumns

--- frontend/src/Hooks/useSearchConfig.js
+++ frontend/src/Hooks/useSearchConfig.js
@@ -6,6 +6,7 @@ function useSearchConfig () {
     const {searchConfig, setSearchConfig} = useContext(MyContext);
 
     const getSearchConfigData = async () => {
+        console.log("getSearchConfigData is called!")
         try {
             const res = await services.getSearchConfig();
             if(res.message == "getSearchConfigData success"){
