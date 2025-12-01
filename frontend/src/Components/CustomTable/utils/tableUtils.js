379af43f375243dc2fc2c9fcd59e21f2f019f885
ABF00711 <lwei99597@gmail.com>
2025-12-01 12:14:14 -0300

    refactor(CustomTable): modify to filter date

--- frontend/src/Components/CustomTable/utils/tableUtils.js
+++ frontend/src/Components/CustomTable/utils/tableUtils.js
@@ -156,8 +156,9 @@ export const evaluateCondition = (cellValue, operator, filterValue, columnType) 
                 const match = set.has(`D:${isoDay}`) || set.has(`M:${yyyy}-${mm}`) || set.has(`Y:${yyyy}`);
                 return operator === 'in' ? match : !match;
             }
-
-            const filterDate = new Date(filterValue);
+            const [year, month, day] = filterValue.split("-");
+            const filterDate = new Date(year, month - 1, day); // *This is just the issue*
+            console.log("filterDate: ", filterDate);
             if (isNaN(filterDate.getTime())) return false;
             const filterDay = new Date(filterDate.getFullYear(), filterDate.getMonth(), filterDate.getDate()).getTime();
             const cellDay = new Date(cellDate.getFullYear(), cellDate.getMonth(), cellDate.getDate()).getTime();
